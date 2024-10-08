055a0383c8e88b57607466fdbacfc5a68e5b995a
Sam-script11 <smurengezi21@gmail.com>
2024-10-08 09:23:13 -0500

    updated format

--- src/main/java/com/pluralsight/BedtimeStorie.java
+++ src/main/java/com/pluralsight/BedtimeStorie.java
@@ -9,39 +9,43 @@ public class BedtimeStorie {
         Scanner keyboard = new Scanner(System.in);
         try {
 
-            System.out.print("Please enter:\n1 - Goldilocks\n2 - Hansel and Gretel\n3 - Mary Had a Little Lamb ");
+            System.out.print("1 - Goldilocks\n2 - Hansel and Gretel\n3 - Mary Had a Little Lamb\n Please select from the options: "); // formatting the stories and option to select
             int option = keyboard.nextInt();
             if(option ==1) {
-                FileInputStream fis = new FileInputStream("src/main/resources/goldilocks.txt");
-
-                Scanner scanner = new Scanner(fis);
+                FileInputStream goldLocks = new FileInputStream("src/main/resources/goldilocks.txt");// created fileInputStream object to a specific file in " "
+                // create a scanner to access the file to be read
+                Scanner scanner = new Scanner(goldLocks);
                 String input;
 
+                int lineNumber = 1;
+                // reading until there's  no more data(lines)
                 while (scanner.hasNextLine()) {
+                    // if the file has the next line, it will read it
                     input = scanner.nextLine();
-                    System.out.println(input);
+                    System.out.println(lineNumber++ +" "+ input.trim());
                 }
                 scanner.close();
             } else if (option ==2){
-                FileInputStream fis = new FileInputStream("src/main/resources/hansel_and_gretel.txt");
+                FileInputStream hanselAndGretel = new FileInputStream("src/main/resources/hansel_and_gretel.txt");
 
-                Scanner scanner = new Scanner(fis);
+                Scanner scanner = new Scanner(hanselAndGretel);
                 String input;
-
+                int lineNumber = 1;
                 while (scanner.hasNextLine()) {
                     input = scanner.nextLine();
-                    System.out.println(input);
+                    System.out.println(lineNumber++ +" "+ input);
+
                 }
                 scanner.close();
             }
             else if( option==3){
-                FileInputStream fis = new FileInputStream("src/main/resources/mary_had_a_little_lamb.txt");
-                Scanner scanner = new Scanner(fis);
+                FileInputStream MaryLittleLamb = new FileInputStream("src/main/resources/mary_had_a_little_lamb.txt");
+                Scanner scanner = new Scanner(MaryLittleLamb);
                 String input;
-
+                int lineNumber =1;
                 while (scanner.hasNextLine()) {
                     input = scanner.nextLine();
-                    System.out.println(input);
+                    System.out.println(lineNumber++ +" "+ input);
                 }
                 scanner.close();
 
@@ -49,7 +53,6 @@ public class BedtimeStorie {
             else{
                 System.out.println("Invalid option");
             }
-
 
         } catch(IOException e){
             e.printStackTrace();
